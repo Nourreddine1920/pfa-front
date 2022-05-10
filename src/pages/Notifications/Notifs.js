35114a01cd30ba0c208c405d856be3526808018d
brahmitarek6 <brahmitarek6@gmail.com>
2022-05-10 21:57:19 +0100

    update notif page

--- src/pages/Notifications/Notifs.js
+++ src/pages/Notifications/Notifs.js
@@ -1,14 +1,12 @@
 import React from "react";
 // Import Components
-import { Col, Row, Card } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 import { withRouter } from "react-router-dom";
-// import Demo from "../Demo/Demo";
-import Demo1 from "../Demo/Demo1";
-import Demo2 from "../Demo/Demo2";
+import "./Notifs.css"
 
 
 export const Notifs = (props) => {
-  
+
   return (
     <div className="main-wrapper login-body">
       <div className="page-header">
@@ -17,7 +15,7 @@ export const Notifs = (props) => {
             <h3 className="page-title"> Notifications</h3>
             <ul className="breadcrumb">
               <li className="breadcrumb-item">
-                <a href="/profile"> User </a>
+                <a href="/profile"> Profile </a>
               </li>
               <li className="breadcrumb-item active">Notifications </li>
             </ul>
@@ -30,84 +28,39 @@ export const Notifs = (props) => {
             <h5 className="card-title"> Student or Teacher Notifications </h5>
           </Card.Header>
           <Card.Body >
-            {/* <ul className="activity-feed notification-list">
-              <li className="feed-item notification-message">
-                <div className="feed-date"> 4 mins ago </div>
-                <span className="feed-text">
-                  <a href="#">
-                    <div
-                      className="media d-flex"
-                      style={{
-                        justifyContent: "space-around",
-                        display: "flex",
-                        backgroundColor: "aliceblue",
-                        borderRadius: "5px 5px 26px",
-                      }}
-                    >
-                      <span className="avatar avatar-sm flex-shrink-0">
-                        <img
-                          className="avatar-img rounded-circle"
-                          alt="User Image"
-                          src="assets/img/profiles/avatar-02.jpg"
-                        />
-                      </span>
-                      <div className="media-body flex-grow-1">
-                        <p className="noti-details">
-                          <span className="noti-title">Carlson Tech</span> has
-                          approved <span className="noti-title">your estimate</span>
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </span>
+            <ul class="timeline">
+              <li class="timeline-event">
+                <label class="timeline-event-icon"></label>
+                <div class="timeline-event-copy">
+                  <p class="timeline-event-thumbnail">April 2011 -EE</p>
+                  <h3>notif title</h3>
+                  <p>
+                    notif subject
+                  </p>
+                </div>
               </li>
-              <li className="feed-item notification-message">
-                <div className="feed-date"> 6 mins ago</div>
-                <span className="feed-text">
-                  <a href="#">
-                    <div className="media d-flex">
-                      <span className="avatar avatar-sm flex-shrink-0">
-                        <img
-                          className="avatar-img rounded-circle"
-                          alt="User Image"
-                          src="assets/img/profiles/avatar-11.jpg"
-                        ></img>
-                      </span>
-                      <div className="media-body flex-grow-1">
-                        <p className="noti-details">
-                          <span className="noti-title">International Software Inc </span> has sent you a invoice in the amount of <span className="noti-title">$218</span>
-                        </p>
-                        
-                      </div>
-                    </div>
-                  </a>
-                </span>
+              <li class="timeline-event">
+                <label class="timeline-event-icon"></label>
+                <div class="timeline-event-copy">
+                  <p class="timeline-event-thumbnail">April 2011 -EE</p>
+                  <h3>notif title</h3>
+                  <p>
+                    notif subject
+                  </p>
+                </div>
               </li>
-              <li className="feed-item notification-message">
-                <div className="feed-date">8 mins ago</div>
-                <span className="feed-text">
-                  <a href="#">
-                    <div className="media d-flex">
-                      <span className="avatar avatar-sm flex-shrink-0">
-                        <img
-                          className="avatar-img rounded-circle"
-                          alt="User Image"
-                          src="assets/img/profiles/avatar-17.jpg"
-                        ></img>
-                      </span>
-                      <div className="media-body flex-grow-1">
-                        <p className="noti-details">
-                          <span className="noti-title">John Hendry</span> sent a cancellation request <span className="noti-title"> Apple iPhone XR</span>
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </span>
+              <li class="timeline-event">
+                <label class="timeline-event-icon"></label>
+                <div class="timeline-event-copy">
+                  <p class="timeline-event-thumbnail">April 2011 -EE</p>
+                  <h3>notif title</h3>
+                  <p>
+                    notif subject
+                  </p>
+                </div>
               </li>
-            </ul> */}
-            {/* <Demo/> */}
-            <Demo1/>
-            {/* <Demo2/> */}
+
+            </ul>
           </Card.Body>
         </Card>
       </Row>
